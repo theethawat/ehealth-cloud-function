0d75e9d4a0ec8caf6e907a36c6ad67569bd34bd1
Theethawat Savastham <5910110150@psu.ac.th>
2020-03-22 13:42:08 +0700

    Add Other Data Type Function

--- index.js
+++ index.js
@@ -31,11 +31,130 @@ exports.addAgeToBP = functions.firestore
         }
         snapshot.forEach(doc => {
           let userCollectionData = doc.data()
-          birthYear = userCollectionData.bYear
+          birthYear = userCollectionData.byear
+          console.log("BirthYear" + birthYear)
         })
-        let date = Date()
-        age = date.getFullYear() - birthYear
+        console.log(moment().year())
+        age = moment().year() - birthYear
+        console.log(age)
         db.collection("blood_pressure")
+          .doc(id)
+          .update({
+            age: age
+          })
+        return Promise
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+exports.addAgeToHR = functions.firestore
+  .document("/heart_rate/{userUUID}")
+  .onCreate((snap, context) => {
+    const newData = snap.data()
+    let ownerUUID = newData.ownerUUID
+    let birthYear
+    let age = 0
+    let id = snap.id
+
+    console.log("OwnerUUID" + ownerUUID)
+
+    db.collection("user")
+      .where("inputProgramUser", "==", ownerUUID)
+      .limit(1)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("Data in User Table is Empty")
+        }
+        snapshot.forEach(doc => {
+          let userCollectionData = doc.data()
+          birthYear = userCollectionData.byear
+          console.log("BirthYear" + birthYear)
+        })
+        console.log(moment().year())
+        age = moment().year() - birthYear
+        console.log(age)
+        db.collection("heart_rate")
+          .doc(id)
+          .update({
+            age: age
+          })
+        return Promise
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+exports.addAgeToGlucose = functions.firestore
+  .document("/glucose/{userUUID}")
+  .onCreate((snap, context) => {
+    const newData = snap.data()
+    let ownerUUID = newData.ownerUUID
+    let birthYear
+    let age = 0
+    let id = snap.id
+
+    console.log("OwnerUUID" + ownerUUID)
+
+    db.collection("user")
+      .where("inputProgramUser", "==", ownerUUID)
+      .limit(1)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("Data in User Table is Empty")
+        }
+        snapshot.forEach(doc => {
+          let userCollectionData = doc.data()
+          birthYear = userCollectionData.byear
+          console.log("BirthYear" + birthYear)
+        })
+        console.log(moment().year())
+        age = moment().year() - birthYear
+        console.log(age)
+        db.collection("glucose")
+          .doc(id)
+          .update({
+            age: age
+          })
+        return Promise
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+exports.addAgeToSPO2 = functions.firestore
+  .document("/spo2/{userUUID}")
+  .onCreate((snap, context) => {
+    const newData = snap.data()
+    let ownerUUID = newData.ownerUUID
+    let birthYear
+    let age = 0
+    let id = snap.id
+
+    console.log("OwnerUUID" + ownerUUID)
+
+    db.collection("user")
+      .where("inputProgramUser", "==", ownerUUID)
+      .limit(1)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("Data in User Table is Empty")
+        }
+        snapshot.forEach(doc => {
+          let userCollectionData = doc.data()
+          birthYear = userCollectionData.byear
+          console.log("BirthYear" + birthYear)
+        })
+        console.log(moment().year())
+        age = moment().year() - birthYear
+        console.log(age)
+        db.collection("spo2")
           .doc(id)
           .update({
             age: age
